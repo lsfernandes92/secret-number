d885e3d6cbaad7db6f33122333fd21e268b013f9
lsfernandes92 <lsfernandes92@gmail.com>
2024-03-11 21:13:36 -0300

    Implement speech recognition

--- speechRecognition.js
+++ speechRecognition.js
@@ -1,5 +1,26 @@
+const divGuess = document.querySelector("#guess")
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition
 
 const recognition = new SpeechRecognition()
-recognition.lang = "pt-Br"
+recognition.lang = "pt-BR"
 recognition.start()
+
+recognition.addEventListener("result", onSpeak)
+
+function onSpeak(e) {
+  console.log(e)
+  guess = e.results[0][0].transcript
+  showGuess(guess)
+}
+
+function showGuess(guess) {
+  const divYouSaid = document.createElement("div")
+  const spanBox = document.createElement("span")
+
+  divYouSaid.innerHTML = `<div>You said:</div>`
+  spanBox.innerHTML = `<span class="box">${guess}</span>`
+
+  divGuess.append(divYouSaid)
+  divGuess.append(spanBox)
+}
